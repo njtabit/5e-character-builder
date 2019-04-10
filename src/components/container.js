ff96f7d8e551f5b3052fb2fc3bcef9bd709d64e3
NJTabit <njtabit@gmail.com>
2019-04-10 15:18:25 -0700

    Changed container from giant switch to object-key loading

--- src/components/container.js
+++ src/components/container.js
@@ -16,43 +16,21 @@ class Container extends Component {
   }
 
   render() {
-    switch (this.props.contents) {
-      case 'Home':
-        return <Home />
-
-      case 'Race':
-        return <Race />
-
-      case 'Class':
-        return <Class />
-
-      case 'Background':
-        return <Background />
-
-      case 'Stats':
-        return <Stats />
-
-      case 'Feats':
-        return <Feats />
-
-      case 'Proficiencies':
-        return <Proficiencies />
-
-      case 'Misc':
-        return <Misc />
-
-      case 'Spells':
-        return <Spells />
-
-      case 'Equipment':
-        return <Equipment />
-
-      case 'Info':
-        return <Info />
-
-      default:
-        return <Home />
+    const tabs = {
+      Home,
+      Race,
+      Class,
+      Background,
+      Stats,
+      Feats,
+      Proficiencies,
+      Misc,
+      Spells,
+      Equipment,
+      Info
     }
+    const Tab = tabs[this.props.contents]
+    return <Tab />
   }
 }
 
