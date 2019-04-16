c0779b42701f1df8f79a8ff5d476e00805b3ad22
NJTabit <njtabit@gmail.com>
2019-04-16 02:16:19 -0700

    fixed a lingering filter issue, messed with selector styles a bit

--- src/components/filters.js
+++ src/components/filters.js
@@ -3,18 +3,25 @@ import Toggle from './toggle'
 import { src } from '../lists/sources.json'
 
 export default (props) => {
+  let srcBlock = []
+  let seriesBlock = []
+
+  props.srclist.map( (source, i) => {
+    srcBlock.push(<Toggle name={ src[source].name } active={ (props.filters[src[source].series] && props.filters[source]) } abbr={ source } toggle={ props.toggle } series={src[source].series} />)
+
+    if (!seriesBlock.includes(src[source].series)) { seriesBlock.push(src[source].series) }
+  })
+
+  seriesBlock.map( (series, i) => {
+    seriesBlock[i] = <Toggle name={src[series].name} active={props.filters[series]} abbr={series} toggle={props.toggle} />
+  })
 
   return <div className="filters">
     <div className="source-type-filters">
-      <Toggle name="Plane Shift" active={props.filters['PS']} abbr="PS" toggle={props.toggle} />
-      <Toggle name="Unearthed Arcana" active={props.filters['UA']} abbr="UA" toggle={props.toggle} />
+      { seriesBlock }
     </div>
     <div className={'source-books PS' + props.filters['PS'] + ' UA' + props.filters['UA']} >
-      {
-        props.srclist.map( (source, i) => {
-          return <Toggle name={ src[source].name } active={ (props.filters[src[source].series] && props.filters[source]) } abbr={ source } toggle={ props.toggle } series={src[source].series} />
-        })
-      }
+      { srcBlock }
     </div>
   </div>
 }
