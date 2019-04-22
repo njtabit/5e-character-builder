a2f0bf906a833d6809fca5b1a25fb0ba636b091f
NJTabit <NJTabit@gmail.com>
2019-04-21 19:42:40 -0700

    Created function to 'toString' infobox objects, adjusted default 1080p font size.

--- src/components/infobox.js
+++ src/components/infobox.js
@@ -1,5 +1,30 @@
 import React from 'react'
 
 export default (props) => {
-  return <div className="infobox">{ props.viewing ? props.viewing.name : 'Infobox' }</div>
+
+  const toList = (info) => {
+
+    const parseObj = (obj) => {
+
+      let list = []
+      for (const key in obj) {
+        list.push( <div key={ key } id={ key } ><span>{ key }: </span><span>{ toList(obj[key]) } </span></div> )
+      }
+      return list
+    }
+
+    const parseArr = (arr) => {
+      let list = []
+
+      for (let i = 0; i < arr.length; i++) {
+        list.push( toList(arr[i]) )
+      }
+      return list
+    }
+
+    if (typeof info === 'string') { return info } else if (Array.isArray(info)) { return parseArr(info) } else if (typeof info === 'object') { return parseObj(info) } else { return info }
+  }
+
+
+  return <div className="infobox"> { toList(props.viewing) } </div>
 }
